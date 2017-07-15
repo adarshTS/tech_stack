0dcafc6056a6521275c8d6860e846a9334b96032
adarshTS <adarsh.ts8@gmail.com>
2017-07-15 10:03:59 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './components/common/Header';
+
+const App = () => {
+  return (
+
+  <View>
+  <Header headerText="Tech Stack" />
+  </View>
+);
+};
+export default App;
