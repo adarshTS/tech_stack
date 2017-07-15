84a4d9edf4c527eab4749f87be736e537f32a87e
adarshTS <adarsh.ts8@gmail.com>
2017-07-15 13:05:48 +0530

    changes after re-reffering

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import Header from './components/common/Header';
 
 const App = () => {
   return (
-
-  <View>
-  <Header headerText="Tech Stack" />
+    <Provider store={createStore(reducers)}>
+    <View>
+    <Header headerText="Tech_Stack" />
   </View>
+  </Provider>
 );
 };
 export default App;
